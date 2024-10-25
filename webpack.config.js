4cca0be2b3625754cc71fbfd3cbc6a57a00df811
Motazfarwa <farwamotez@gmail.com>
2024-10-25 14:18:33 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
-const path = require('path');
+name: Node.js CI
 
-module.exports = {
-  entry: './app.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
-  resolve: {
-    fallback: {
-      path: require.resolve('path-browserify'),
-    },
-  },
-};
+on:
+  push:
+    branches: [ "main" ]
+  pull_request:
+    branches: [ "main" ]
+
+jobs:
+  build:
+    runs-on: ubuntu-latest
+
+    strategy:
+      matrix:
+        node-version: [18.x, 20.x, 22.x]
+
+    steps:
+      - uses: actions/checkout@v4
+      - name: Use Node.js ${{ matrix.node-version }}
+        uses: actions/setup-node@v4
+        with:
+          node-version: ${{ matrix.node-version }}
+          cache: 'npm'
+          
+      # Check if dependencies are missing and install them if necessary
+      - name: Install dependencies if not present
+        run: |
+          if [ ! -d "node_modules" ]; then
+            npm install
+          else
+            echo "Dependencies already installed."
+          fi
+
+      - run: npm run build --if-present
+      - run: npm test
