a3e9c079b4a702bbb11d781a5c397edc429da41e
Motazfarwa <farwamotez@gmail.com>
2024-10-25 14:09:19 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: './app.js', // Adjust this path if your entry file is elsewhere
+  entry: './app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // Output directory
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: 'production', // You can change this to 'development' if needed
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader', // Install if using ES6/React
+          loader: 'babel-loader',
         },
       },
     ],
   },
   resolve: {
     fallback: {
-      "path": require.resolve("path-browserify"),
-      "zlib": require.resolve("browserify-zlib"),
-      "querystring": require.resolve("querystring-es3"),
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "http": require.resolve("stream-http"),
-      "fs": false, // Use empty module for 'fs' since it's not needed in browser
-      "net": false, // Use empty module for 'net'
+      path: require.resolve('path-browserify'),
     },
   },
 };
